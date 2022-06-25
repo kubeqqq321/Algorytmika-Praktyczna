07ddd5bec6198c308a8864856bba50da72f06eb1
JPM <58236757+kubeqqq321@users.noreply.github.com>
2022-06-25 12:49:13 +0200

    Add files via upload

---
+++ Zajęcia_2/AP_MarciniakJ_adding.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+ int reverse(int x)
+{
+    int ans = 0;
+    while(x !=0)
+    {
+        ans = ans * 10 + x % 10;
+        x /= 10;
+    }
+    return ans;
+}
+
+int main()
+{
+    int ilosc_przypadkow, liczba1, liczba2;
+    std::cin >> ilosc_przypadkow;
+
+    while(ilosc_przypadkow--)
+    {
+        std::cin >> liczba1 >> liczba2;
+        const int suma = reverse(liczba1) + reverse(liczba2);
+        std::cout << reverse(suma) << std::endl;
+    }
+
+}
