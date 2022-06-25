4e9ec8b89046a4ef4ea72ed9215ebdeddcce272d
JPM <58236757+kubeqqq321@users.noreply.github.com>
2022-06-25 12:49:29 +0200

    Add files via upload

---
+++ Zajęcia_2/AP_MarciniakJ_z2glass.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <string>
+
+
+void solve()
+{
+	std::string tekst;
+	std::cin >> tekst;
+
+	int i = 0;
+	int j = 1;
+	int k = 0;
+	int dlug = tekst.length();
+
+	while ( i < dlug && j < dlug && k < dlug)
+	{
+		int t = tekst[(i + k) % dlug] - tekst[(j + k) % dlug];
+
+		if (t == 0) ++k;
+		else if (t < 0)
+		{
+			j += k + 1;
+			k = 0;
+		}
+		else
+		{
+			i += k + 1;
+			k = 0;
+		}
+		if(i == j)
+			j++;
+	}
+	int odp;
+	if (i < j)
+		odp = i;
+	else
+		odp = j;
+	std::cout << odp + 1 << std::endl;
+
+}
+
+int main()
+{
+	int ile_prob;
+	std::cin >> ile_prob;
+	while(ile_prob--)
+	{
+		solve();
+	}
+	return 0;
+}
+
